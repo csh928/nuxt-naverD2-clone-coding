89f46b644b159520686ef264a1a84e15d109ba6d
204113 <204113@IC0-W-N11713.ssg.global>
2021-03-04 17:32:33 +0900

    첫번째

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavBar />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/NavBar";
+import Footer from "@/components/Footer";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
